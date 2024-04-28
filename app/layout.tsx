a78e924b704276304d0a70d8f530b88975ddc2ff
bellobambo <bellobambo21@gmail.com>
2024-04-28 01:17:34 +0100

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="max-w-3xl mx-auto text-slate-800">
+          <header className="p-6 border-b flex bg-blue-500 justify-between rounded-br-lg items-center">
+            <Link className="text-2xl font-bold text-white" href={'/'}>Tech Interpretations</Link>
+            <Link className="bg-slate-100 grid place-items-center py-2 px-4 rounded-full shadow-md" href={'/create'}>Add New</Link>
+          </header>
+
+          <main className="p-4 text-lg">
+            {children}
+
+          </main>
+        </div>
+      </body>
     </html>
   );
 }
