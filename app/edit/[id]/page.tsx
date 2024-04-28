2162642fcfe7fc8255b03ff8f7824f5c7a807f88
bellobambo <bellobambo21@gmail.com>
2024-04-28 22:09:57 +0100

    test

---
+++ app/edit/[id]/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const EditPage = () => {
+  return (
+    <div>
+    <h2 className='text-2xl font-bold my-8 '>Edit Interpretation</h2>
+    <form action="" className='flex gap-3 flex-col '>
+        <input type="text" name='term' placeholder='term' className='py-1 px-4 border rounded-md  ' />
+
+        <textarea name="interpretation" rows={4} placeholder='Interpretation' className='py-1 px-4 border rounded-md resize-none'></textarea>
+
+        <button className='bg--black text-white mt-5 px-4 py-1 rounded-md cursor-pointer'>Add Interpretation</button>
+    </form>
+</div>
+  )
+}
+
+export default EditPage
--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,19 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <div className="font-bold p-4 my-2 rounded-md leading-8">
+      <div className="font-bold p-4 rounded-md leading-8">
         Natural Language Processing (NLP)
       </div>
-      <div>
+      <div className="p-4">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, iusto! Culpa cupiditate, dolores officia excepturi eligendi rem voluptates iste, ducimus minima incidunt deleniti, ad corporis blanditiis velit sunt facere nihil?
       </div>
 
-      <div>
-        <Link className="bg-slate-300 px-4 py-2 " href={'/edit'}> Edit </Link>
+      <div className="flex gap-4 mt-4 justify-end ">
+        <Link className="bg-slate-300 px-4 py-2 uppercase text-sm font-bold tracking-widest" href={'/edit'}> Edit </Link>
+
+        <button className="bg-red-500 text-white px-4 py-2 uppercase text-sm font-bold tracking-widest">
+Delete
+        </button>
       </div>
     </div>
   );
