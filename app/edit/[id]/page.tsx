351cf975d929c3db565a2b182543e568cf5311d8
bellobambo <bellobambo21@gmail.com>
2024-04-30 00:16:40 +0100

    final

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -1,17 +1,91 @@
-import React from 'react'
+'use client'
 
-const EditPage = () => {
+import { useRouter } from 'next/navigation'
+import React, { ChangeEvent, useEffect, useState } from 'react'
+
+
+const EditPage = ({ params }: { params: { id: string } }) => {
+  const [formData, setFormData] = useState({ term: "", interpretation: "" })
+  const [isLoading, setisLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`/api/interpretations/${params.id}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch interpretation')
+        }
+
+        const data = await response.json();
+        setFormData({ term: data.interpretation.term, interpretation: data.interpretation.interpretation })
+      } catch (error) {
+        setError('Failed to load interpretations')
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setFormData((prevData) => (
+      {
+        ...prevData,
+        [e.target.name]: e.target.value,
+      }
+    ));
+
+
+  }
+
+  const router = useRouter()
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!formData.term || !formData.interpretation) {
+      setError("Please Fill in All the fields");
+      return;
+    }
+
+    setError(null);
+    setisLoading(true);
+
+    try {
+      const response = await fetch(`/api/interpretations/${params.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to update interpretation");
+      }
+
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+      setError("Something went wrong");
+    } finally {
+      setisLoading(false);
+    }
+  };
   return (
     <div>
-    <h2 className='text-2xl font-bold my-8 '>Edit Interpretation</h2>
-    <form action="" className='flex gap-3 flex-col '>
-        <input type="text" name='term' placeholder='term' className='py-1 px-4 border rounded-md  ' />
+      <h2 className='text-2xl font-bold my-8 '>Edit Interpretation</h2>
+      <form action="" onSubmit={handleSubmit} className='flex gap-3 flex-col '>
+        <input type="text" value={formData.term} onChange={handleInputChange} name='term' placeholder='term' className='py-1 px-4 border rounded-md  ' />
 
-        <textarea name="interpretation" rows={4} placeholder='Interpretation' className='py-1 px-4 border rounded-md resize-none'></textarea>
+        <textarea name="interpretation" value={formData.interpretation} onChange={handleInputChange} rows={4} placeholder='Interpretation' className='py-1 px-4 border rounded-md resize-none'></textarea>
 
-        <button className='bg--black text-white mt-5 px-4 py-1 rounded-md cursor-pointer'>Add Interpretation</button>
-    </form>
-</div>
+        <button className='bg-black text-white mt-5 px-4 py-1 rounded-md cursor-pointer'>{isLoading ? 'Updating...' : 'Update Interpretation'}</button>
+
+        {error && <p className='text-red-500 mt-4'>{error}</p>}
+
+      </form>
+    </div>
   )
 }
 
